e2840cff44cd6b257387a09db48518b689c74109
Gonçalo Passos <dev@diogopassos.pt>
2021-02-18 21:30:21 +0000

    Add DnsAddressRetriever class

---
+++ Retriever/DnsAddressRetriever.cs
@@ -0,0 +1,31 @@
+﻿using DnsClient; 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynClient.Retriever
+{
+    class DnsAddressRetriever : IAddressRetriever
+    {
+        private LookupClient _client;
+
+        private const String openDnsNs1 = "208.67.222.222";
+        private const String openDnsNs2 = "208.67.220.220";
+
+        public DnsAddressRetriever()
+        {
+            _client = new LookupClient(IPAddress.Parse(openDnsNs1),IPAddress.Parse(openDnsNs2));
+        }
+
+        public IPAddress GetAddress()
+        {
+            var result = _client.Query("myip.opendns.com", QueryType.A);
+            var record = result.Answers.ARecords().FirstOrDefault();
+           
+            return record?.Address;
+        }
+    }
+}
