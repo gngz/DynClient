da89f347be5febc05e4ab0770bf89aac9a8a1308
Gonçalo Passos <dev@diogopassos.pt>
2021-02-18 21:30:33 +0000

    Add RetrieverFactory

---
+++ Retriever/RetrieverFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynClient.Retriever
+{
+
+    public enum RetrieverType { Http, Dns};
+    class RetrieverFactory
+    {
+        public static IAddressRetriever GetRetriever(RetrieverType type)
+        {
+            if (type.Equals(RetrieverType.Dns)) return new DnsAddressRetriever();
+
+            return new HttpAddressRetriever();
+
+        }
+    }
+}
