f5594ddd880b08fb65482d9f87a7e5316184bc39
Gonçalo Passos <dev@diogopassos.pt>
2021-02-19 22:26:42 +0000

    Add Author and Logger Attributes into IProviderPlugin

--- Plugins/IProviderPlugin.cs
+++ Plugins/IProviderPlugin.cs
@@ -1,15 +1,16 @@
-﻿using System;
+﻿using DynClient.Logger;
+using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace DynClient.Plugins
 {
     public interface IProviderPlugin
     {
         String Name { get; }
+        String Author { get; }
+        IDynLogger Logger { get; set; }
         List<String> OptionRequirements { get; set; }
         void Update(IPAddress address, IDictionary<String,Object> config);
 
